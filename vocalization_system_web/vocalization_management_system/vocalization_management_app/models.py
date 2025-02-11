b3f07fb332880807b2173a1f2b5509afd37d97a8
rhaque23 <rhaque287@gmail.com>
2025-02-11 17:53:18 -0500

    Add tables

--- vocalization_system_web/vocalization_management_system/vocalization_management_app/models.py
+++ vocalization_system_web/vocalization_management_system/vocalization_management_app/models.py
@@ -48,3 +48,82 @@ class AudioFile(models.Model):
 
     def __str__(self):
         return f"{self.animal_type} - {self.file.name}"
+
+class ZooTable(models.Model): 
+    zoo_id = models.IntegerField(auto_created=True)
+    zoo_name = models.CharField(max_length=20)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    contact_email = models.CharField(max_length=20)
+
+    def _str_(self): 
+        return self.zoo_name
+
+class AnimalTable(models.Model):
+    animal_id = models.IntegerField(auto_created=True)
+    species_name = models.CharField(max_length=20)
+    zoo_id = models.IntegerField(auto_created=True)
+
+    def _str_(self):
+        return self.species_name
+
+class OriginalAudioFile(models.Model):
+    ANIMAL_CHOICES = [
+        ('amur_leopard', 'Amur Leopard'),
+        ('amur_tiger', 'Amur Tiger'),
+    ]
+
+    file_id = models.IntegerField(auto_created=True)
+    audio_file_name = models.CharField(max_length=20)
+    recording_date = models.DateTimeField()
+    animal_type = models.CharField(max_length=20, choices=ANIMAL_CHOICES)
+    animal = models.IntegerField(auto_created=True)
+    zoo = models.IntegerField(auto_created=True)
+    file_size_mb = models.FloatField(auto_created=True)
+    upload_date = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.animal_type} - {self.audio_file_name}"
+
+class ReducedAudioFile(models.Model):
+    ANIMAL_CHOICES = [
+        ('amur_leopard', 'Amur Leopard'),
+        ('amur_tiger', 'Amur Tiger'),
+    ]
+
+    file_id = models.IntegerField(auto_created=True)
+    audio_file_name = models.CharField(max_length=20)
+    recording_date = models.DateTimeField()
+    animal_type = models.CharField(max_length=20, choices=ANIMAL_CHOICES)
+    animal = models.IntegerField(auto_created=True)
+    zoo = models.IntegerField(auto_created=True)
+    file_size_mb = models.FloatField(auto_created=True)
+    upload_date = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.animal_type} - {self.audio_file_name}"
+
+class DetectedNoiseAudioFile(models.Model):
+    detected_noise_file_id = models.IntegerField(auto_created=True)
+    original_file_id = models.IntegerField(OriginalAudioFile.file_id)
+    detected_noise_file_path = models.CharField(max_length=100)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    saw_count = models.IntegerField(blank=False)
+    saw_call_count = models.IntegerField(blank=False)
+    noise_verfied = models.BooleanField(saw_count > 0 or saw_call_count > 0)
+    file_size_mb = models.FloatField(auto_created=True)
+    upload_date = models.DateTimeField(default=now)
+
+    def _str_(self):
+        return self.detected_noise_file_path
+
+
+
+# class is table
+# each row under class is a column in the table 
+    # ex: user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    #     full_name = models.CharField(max_length=100)
+    #     department = models.CharField(max_length=100, null=True, blank=True)
+    #     created_at = models.DateTimeField(auto_now_add=True)
+
+    
