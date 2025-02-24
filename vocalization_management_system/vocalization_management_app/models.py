5d660230457c7b7afb5cedc76d486dc5c2f7e7dc
studentluke <112911552+studentluke@users.noreply.github.com>
2025-02-24 18:56:15 -0500

    quick and dirty azure updates

--- vocalization_management_system/vocalization_management_app/models.py
+++ vocalization_management_system/vocalization_management_app/models.py
@@ -45,6 +45,11 @@ class Zoo(models.Model):
     location = models.CharField(max_length=255, blank=True, null=True)
     contact_email = models.EmailField(unique=True)
 
+    # to improve performance when querying, might not be necessary
+    # class Meta:
+    #     indexes = [
+    #         models.Index(fields=['zoo_name'])
+    #     ]
     def __str__(self):
         return self.zoo_name
 
@@ -103,17 +108,17 @@ class OriginalAudioFile(models.Model):
 
 
 # Processed & Reduced Audio File Table
-class ProcessedAudioFile(models.Model):
-    file_id = models.AutoField(primary_key=True)
-    audio_file_name = models.CharField(max_length=100)
-    recording_date = models.DateTimeField()
-    original_file = models.ForeignKey(OriginalAudioFile, on_delete=models.CASCADE, related_name="processed_audio")
-    file_size_mb = models.FloatField()
-    processed_by = models.ForeignKey(StaffProfile, on_delete=models.CASCADE, null=True, blank=True)
-    upload_date = models.DateTimeField(default=now)
+# class ProcessedAudioFile(models.Model):
+#     file_id = models.AutoField(primary_key=True)
+#     audio_file_name = models.CharField(max_length=100)
+#     recording_date = models.DateTimeField()
+#     original_file = models.ForeignKey(OriginalAudioFile, on_delete=models.CASCADE, related_name="processed_audio")
+#     file_size_mb = models.FloatField()
+#     processed_by = models.ForeignKey(StaffProfile, on_delete=models.CASCADE, null=True, blank=True)
+#     upload_date = models.DateTimeField(default=now)
 
-    def __str__(self):
-        return f"Processed: {self.audio_file_name}"
+#     def __str__(self):
+#         return f"Processed: {self.audio_file_name}"
 
 
 # Detected Noise in Audio Files
@@ -132,6 +137,22 @@ class DetectedNoiseAudioFile(models.Model):
     def save(self, *args, **kwargs):
         self.noise_verified = self.saw_count > 0 or self.saw_call_count > 0
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Detected Noise: {self.detected_noise_file_path}"
+    
+# Detected Noise in Audio Files
+class AutoDetectedNoiseAudioFile(models.Model):
+    detected_noise_file_id = models.AutoField(primary_key=True)
+    original_file = models.ForeignKey(OriginalAudioFile, on_delete=models.CASCADE, related_name="detected_noises")
+    detected_noise_file_path = models.CharField(max_length=255)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    saw_count = models.IntegerField()
+    saw_call_count = models.IntegerField()
+    file_size_mb = models.FloatField()
+    upload_date = models.DateTimeField(default=now)
+    noise_verified = True
 
     def __str__(self):
         return f"Detected Noise: {self.detected_noise_file_path}"
@@ -162,6 +183,16 @@ class Waveform(models.Model):
 
     def __str__(self):
         return f"Waveform for {self.audio_file.audio_file_name}"
+    
+# Fourier Transform Table
+class Waveform(models.Model):
+    audio_file = models.ForeignKey(OriginalAudioFile, on_delete=models.CASCADE, related_name='stfts')
+    image_path = models.ImageField(upload_to='stfts/')
+    generated_by = models.ForeignKey(StaffProfile, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Fourier Transform for {self.audio_file.audio_file_name}"
 
 
 # Audio Processing Table (Processes Audio Files)
